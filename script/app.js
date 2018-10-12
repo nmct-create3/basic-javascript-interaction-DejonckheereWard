9b69909880a68b68060ca7efa6064b66b1f1aca5
DejonckheereWard <ward.dejonckheere@student.howest.be>
2018-10-12 15:45:57 +0200

    Range input / slider afgewerkt

--- script/app.js
+++ script/app.js
@@ -1,9 +1,12 @@
+"use strict"
+
 document.addEventListener( 'DOMContentLoaded', function(){
 
         console.log( 'Script loaded!' );
 
         handleFloatingLabel();
         handlePasswordSwitcher();
+        handleQualityRange();
 
     });
 
@@ -12,15 +15,15 @@ function handleFloatingLabel(){
   let input = document.querySelector(".js-name-input");
 
   input.addEventListener("blur", function(){
-    console.log("Blur detected")
+    console.log("Blur detected");
     if(input.value != ""){
-      console.log("Class added")
+      console.log("Class added");
       label.classList.add("is-floating");
     } else{
-      console.log("Class removed")
+      console.log("Class removed");
       label.classList.remove("is-floating");
     }
-  })
+  });
 };
 
 function handlePasswordSwitcher() {
@@ -28,16 +31,25 @@ function handlePasswordSwitcher() {
   let password__checkbox = document.querySelector("#toggleCheckbox");
 
   password__checkbox.addEventListener("click", function(){
-    console.log("Password visibility clicked")
-    console.log(password.type)
+    console.log("Password visibility clicked");
+    console.log(password.type);
     if (password.type == "text"){
-      console.log("Password hidden")
-      password.type = "password"
+      console.log("Password hidden");
+      password.type = "password";
     }
     else{
-      console.log("Password visible")
-      password.type = "text"
+      console.log("Password visible");
+      password.type = "text";
     }
-  })
-
+  });
 }
+
+function handleQualityRange() {
+  let rangeInput = document.querySelector(".js-quality-input");
+  let rangeHolder = document.querySelector(".js-range-holder");
+  rangeInput.addEventListener("input", function(){
+    rangeHolder.setAttribute('data-value', rangeInput.value);
+    rangeHolder.style.marginleft = '${rangeinput.value}%';    
+    console.info(rangeInput.value)
+  });
+};
